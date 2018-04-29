c7d44535ae7f3a55757bb99053a3930254bda18d
amukthamalyada <amukthamalyada@users.noreply.github.com>
2018-04-29 02:40:20 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <stdio.h>
+#include <omp.h>
+#include <stdlib.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+   unsigned long long number_circle = 0;
+   unsigned long long number_tosses = 100000000;
+
+#pragma omp parallel for num_threads(12) reduction(+:number_circle)
+   for (unsigned long long toss = 0; toss < number_tosses; toss++) {
+       double x = (double) rand() * 2 / (double) (RAND_MAX) - 1;
+       double y = (double) rand() * 2 / (double) (RAND_MAX) - 1;
+       double distance_squar = x * x + y * y;
+       if (distance_squar <= 1) number_circle++;
+   }
+
+
+   printf("Pi estimation: %f", 4 * number_circle / ((double) number_tosses));
+   return 0;
 }
